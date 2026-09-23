e6f8e535182d3f8f1123f0707e51fa58c0d8126f
Eric Pruitt <eric.pruitt@gmail.com>
2026-09-23 11:54:20 -0500

    Basic conversion to Python 3 with some cleanups
    This updates the program to run on Python 3. Most of the core logic is
    the same, but the logic for parsing the output of "rsync --list-only"
    has been updated to be far more robust than the original implementation
    which should resolve <https://github.com/zaddach/fuse-rsync/issues/2>
    and likely <https://github.com/zaddach/fuse-rsync/issues/1>. This change
    also includes some formatting and style cleanups, mostly for whitespace.

--- fuse_rsync.py
+++ fuse_rsync.py
@@ -1,12 +1,11 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 
-#Copyright (c) 2014 Jonas Zaddach
-#Licensed under the MIT License (https://github.com/zaddach/fuse-rsync/blob/master/LICENSE)
+# Copyright (c) 2014 Jonas Zaddach
+# Licensed under the MIT License (https://github.com/zaddach/fuse-rsync/blob/master/LICENSE)
 
 import os
 import sys
 import errno
-import optparse
 import logging
 import subprocess
 import stat
@@ -17,22 +16,22 @@ import fuse
 from tempfile import mkstemp
 from threading import Lock
 
-fuse.fuse_python_api = (0, 2)  
+fuse.fuse_python_api = (0, 2)
 log = logging.getLogger('fuse_rsync')
 
 class RsyncModule():
     """
         This class implements access to an Rsync module.
     """
-    def __init__(self, host, module, user = None, password = None):
+    def __init__(self, host, module, user=None, password=None):
         self._environment = os.environ.copy()
         self._remote_url = "rsync://"
-        if not user is None:
+        if user is not None:
             self._remote_url += user + "@"
-            
+
         self._remote_url += host + "/" + module
-        
-        if not password is None:
+
+        if password is not None:
             self._environment['RSYNC_PASSWORD'] = password
         self._attr_cache = {}
 
@@ -42,14 +41,14 @@ class RsyncModule():
         """
         result = 0
         if attrs[0] == 'd':
-            result |= stat.S_IFDIR 
+            result |= stat.S_IFDIR
         elif attrs[0] == 'l':
             result |= stat.S_IFLNK
         elif attrs[0] == '-':
             result |= stat.S_IFREG
         else:
-           assert(False)
-            
+           assert False
+
         for i in range(0, 3):
             val = 0
             if 'r' in attrs[1 + 3 * i: 4 + 3 * i]:
@@ -59,44 +58,49 @@ class RsyncModule():
             if 'x' in attrs[1 + 3 * i: 4 + 3 * i]:
                 val |= 1
             result |= val << ((2 - i) * 3)
-            
+
         return result
-        
-    def list(self, path = '/'):
+
+    def list(self, path='/'):
         """
             List files contained in directory __path__.
             Returns a list of dictionaries with keys *attrs* (numerical attribute
             representation), *size* (file size), *timestamp* (File's atime timestamp
             in a datetime object) and *filename* (The file's name).
         """
-        # See http://stackoverflow.com/questions/10323060/printing-file-permissions-like-ls-l-using-stat2-in-c for modes
-        RE_LINE = re.compile("^([ldcbps-]([r-][w-][x-]){3})\s+([0-9]+)\s+([0-9]{4}/[0-9]{2}/[0-9]{2} [0-9]{2}:[0-9]{2}:[0-9]{2}) (.*)$")
         remote_url = self._remote_url + path
         try:
             cmdline = ["rsync", "--list-only", remote_url]
             log.debug("executing %s", " ".join(cmdline))
-            output = subprocess.check_output(["rsync", "--list-only", remote_url], env = self._environment)
+            output = subprocess.check_output(cmdline, env=self._environment, text=True)
 
             listing = []
-            for line in output.decode(encoding = 'iso-8859-1').split("\n"):
-                match = RE_LINE.match(line)
+            for line in output.splitlines():
+                attrs, size_str, date_str, time_str, filename = line.split(None, 4)
 
-                if not match:
+                try:
+                    size = int(size_str.replace(',', ''))
+                    timestamp = datetime.datetime.strptime(
+                        f"{date_str} {time_str}", "%Y/%m/%d %H:%M:%S"
+                    )
+
+                    listing.append({
+                        "attrs": self._parse_attrs(attrs),
+                        "size": size,
+                        "timestamp": timestamp,
+                        "filename": filename
+                    })
+                except (ValueError, AssertionError):
+                    log.warn("Unable to parse line: %r", line)
                     continue
 
-                listing.append({
-                        "attrs": self._parse_attrs(match.group(1)),
-                        "size": int(match.group(3)),
-                        "timestamp": datetime.datetime.strptime(match.group(4), "%Y/%m/%d %H:%M:%S"),
-                        "filename": match.group(5)
-                        })
             return listing
         except subprocess.CalledProcessError as err:
             if err.returncode == 23:
                 return []
             raise err
-           
-    def copy(self, remotepath = '/', localpath = None):
+
+    def copy(self, remotepath='/', localpath=None):
         """
             Copy a file from the remote rsync module to the local filesystem.
             If no local destination is specified in __localpath__, a temporary
@@ -105,12 +109,12 @@ class RsyncModule():
         """
         remote_url = self._remote_url + remotepath
         if localpath is None:
-            (file, localpath) = mkstemp()
-            os.close(file)
+            fd, localpath = mkstemp()
+            os.close(fd)
         cmdline = ["rsync", "--copy-links", remote_url, localpath]
         log.debug("executing %s", " ".join(cmdline))
-        subprocess.check_call(cmdline, env = self._environment)
-        
+        subprocess.check_call(cmdline, env=self._environment)
+
         return localpath
 
 class FuseRsyncFileInfo(fuse.FuseFileInfo):
@@ -118,7 +122,7 @@ class FuseRsyncFileInfo(fuse.FuseFileInfo):
         Encapsulates the file handle for an opened file.
     """
     def __init__(self, handle, **kw):
-        super(FuseRsyncFileInfo, self).__init__(**kw)
+        super().__init__(**kw)
         self.keep = True
         self.handle = handle
 
@@ -136,92 +140,73 @@ class FuseRsync(fuse.Fuse):
         self._attr_cache = {}
         self._file_cache = {}
         self._file_cache_lock = Lock()
-        
-        fuse.Fuse.__init__(self, *args, **kw)
-        
-        self.parser.add_option(mountopt = 'user', default = None, help = "Rsync user on the remote host")
-        self.parser.add_option(mountopt = 'password', type = str, default = None, help = "Rsync password on the remote host")
-        self.parser.add_option(mountopt = 'host', type = str, help = "Rsync remote host")
-        self.parser.add_option(mountopt = 'module', type = str, help = "Rsync module on remote host")
-        self.parser.add_option(mountopt = 'path', type = str, default = "/", help = "Rsync path in module on remote host that is supposed to be the root point")
 
-    # Helpers
-    # =======
-            
+        super().__init__(*args, **kw)
+
+        self.parser.add_option(mountopt='user', default=None, help="Rsync user on the remote host")
+        self.parser.add_option(mountopt='password', type=str, default=None, help="Rsync password on the remote host")
+        self.parser.add_option(mountopt='host', type=str, help="Rsync remote host")
+        self.parser.add_option(mountopt='module', type=str, help="Rsync module on remote host")
+        self.parser.add_option(mountopt='path', type=str, default="/", help="Rsync path in module on remote host that is supposed to be the root point")
+
     def _full_path(self, partial):
         if partial.startswith("/"):
             partial = partial[1:]
         path = os.path.join(self.path, partial)
         return path
-        
+
     def init(self):
         options = self.cmdline[0]
         log.debug("Invoked fsinit() with host=%s, module=%s, user=%s, password=%s", options.host, options.module, options.user, options.password)
         self._rsync = RsyncModule(options.host, options.module, options.user, options.password)
-
-    # Filesystem methods
-    # ==================
-
-    #def access(self, path, mode):
-        #full_path = self._full_path(path)
-        #if not os.access(full_path, mode):
-            #raise FuseOSError(errno.EACCES)
-
-    #def chmod(self, path, mode):
-        #full_path = self._full_path(path)
-        #return os.chmod(full_path, mode)
-
-    #def chown(self, path, uid, gid):
-        #full_path = self._full_path(path)
-        #return os.chown(full_path, uid, gid)
 
     def getattr(self, path, fh=None):
         try:
             log.debug("Invoked getattr('%s')", path)
 
             path = self._full_path(path)
-        
-            st = fuse.Stat() 
-            
+
+            st = fuse.Stat()
+
             if path == "/":
                 st.st_atime = int(time.time())
                 st.st_ctime = int(time.time())
-                st.st_mode  = stat.S_IFDIR | 0555
+                st.st_mode  = stat.S_IFDIR | 0o555
                 st.st_mtime = int(time.time())
                 st.st_nlink = 2
                 st.st_uid = os.geteuid()
                 st.st_gid = os.getegid()
                 return st
-            
+
             if path in self._attr_cache:
                 info = self._attr_cache[path]
             else:
                 listing = self._rsync.list(path)
                 if len(listing) != 1:
-                    log.warn("Found none or several files for path")
+                    log.warning("Found none or several files for path")
                     return -errno.ENOENT
                 info = listing[0]
                 self._attr_cache[path] = info
 
-            timestamp = (info["timestamp"] - datetime.datetime(1970,1,1)).total_seconds()
+            timestamp = (info["timestamp"] - datetime.datetime(1970, 1, 1)).total_seconds()
             st.st_atime = timestamp
             st.st_ctime = timestamp
             st.st_uid = os.geteuid()
             st.st_gid = os.getegid()
             if info["attrs"] & stat.S_IFDIR:
-                st.st_mode  = stat.S_IFDIR | 0555
+                st.st_mode  = stat.S_IFDIR | 0o555
             else:
-                st.st_mode = stat.S_IFREG | 0444
+                st.st_mode = stat.S_IFREG | 0o444
             st.st_mtime = timestamp
             st.st_nlink = 1
             st.st_size = info["size"]
-            
+
             return st
-        except Exception as ex:
+        except Exception:
             log.exception("while doing getattr")
             return -errno.ENOENT
 
-    def readdir(self, path, offset): 
+    def readdir(self, path, offset):
         try:
             if not path.endswith("/"):
                 path += "/"
@@ -231,67 +216,24 @@ class FuseRsync(fuse.Fuse):
 
             yield fuse.Direntry('.')
             yield fuse.Direntry('..')
-        
+
             for dirent in self._rsync.list(full_path):
                 if dirent["filename"] == ".":
                     continue
                 self._attr_cache[path + dirent["filename"]] = dirent
                 yield fuse.Direntry(str(dirent["filename"]))
-        except Exception as ex:
+        except Exception:
             log.exception("While doing readdir")
-
-    #def readlink(self, path):
-        #pathname = os.readlink(self._full_path(path))
-        #if pathname.startswith("/"):
-            ## Path name is absolute, sanitize it.
-            #return os.path.relpath(pathname, self.root)
-        #else:
-            #return pathname
-
-    #def mknod(self, path, mode, dev):
-        #return os.mknod(self._full_path(path), mode, dev)
-
-    #def rmdir(self, path):
-        #full_path = self._full_path(path)
-        #return os.rmdir(full_path)
-
-    #def mkdir(self, path, mode):
-        #return os.mkdir(self._full_path(path), mode)
-
-    #def statfs(self, path):
-        #full_path = self._full_path(path)
-        #stv = os.statvfs(full_path)
-        #return dict((key, getattr(stv, key)) for key in ('f_bavail', 'f_bfree',
-            #'f_blocks', 'f_bsize', 'f_favail', 'f_ffree', 'f_files', 'f_flag',
-            #'f_frsize', 'f_namemax'))
-
-    #def unlink(self, path):
-        #return os.unlink(self._full_path(path))
-
-    #def symlink(self, target, name):
-        #return os.symlink(self._full_path(target), self._full_path(name))
-
-    #def rename(self, old, new):
-        #return os.rename(self._full_path(old), self._full_path(new))
-
-    #def link(self, target, name):
-        #return os.link(self._full_path(target), self._full_path(name))
-
-    #def utimens(self, path, times=None):
-        #return os.utime(self._full_path(path), times)
-
-    ## File methods
-    ## ============
 
     def open(self, path, flags):
         log.debug("invoking open(%s, %d)", path, flags)
 
         full_path = self._full_path(path)
-        if flags & (os.O_RDONLY | os.O_WRONLY | os.O_RDWR) != os.O_RDONLY:
+        if (flags & (os.O_RDONLY | os.O_WRONLY | os.O_RDWR)) != os.O_RDONLY:
             return -errno.EACCES
 
         with self._file_cache_lock:
-            if not path in self._file_cache:
+            if path not in self._file_cache:
                 localfile = self._rsync.copy(full_path)
                 self._file_cache[path] = {"refcount": 1, "localpath": localfile}
             else:
@@ -302,26 +244,10 @@ class FuseRsync(fuse.Fuse):
         log.debug("Created file handle %d", handle)
         return FuseRsyncFileInfo(handle)
 
-    #def create(self, path, mode, fi=None):
-        #full_path = self._full_path(path)
-        #return os.open(full_path, os.O_WRONLY | os.O_CREAT, mode)
-
     def read(self, path, length, offset, fh):
         log.debug("invoking read(%s, %d, %d, %d)", path, length, offset, fh.handle)
         os.lseek(fh.handle, offset, os.SEEK_SET)
         return os.read(fh.handle, length)
-
-    #def write(self, path, buf, offset, fh):
-        #os.lseek(fh, offset, os.SEEK_SET)
-        #return os.write(fh, buf)
-
-    #def truncate(self, path, length, fh=None):
-        #full_path = self._full_path(path)
-        #with open(full_path, 'r+') as f:
-            #f.truncate(length)
-
-    #def flush(self, path, fh):
-        #return os.fsync(fh)
 
     def release(self, path, dummy, fh):
         log.debug("invoking release(%s, %d, %d)", path, dummy, fh.handle)
@@ -334,16 +260,12 @@ class FuseRsync(fuse.Fuse):
                 del self._file_cache[path]
                 os.unlink(localfile)
 
-    #def fsync(self, path, fdatasync, fh):
-        #return self.flush(path, fh)
-
 if __name__ == '__main__':
-    fs = FuseRsync()  
-    fs.parse(errex=1)  
-#TODO: Below is hacky, find properly parsed debug attribute
+    fs = FuseRsync()
+    fs.parse(errex=1)
     if '-d' in sys.argv:
-        logging.basicConfig(level = logging.DEBUG)
+        logging.basicConfig(level=logging.DEBUG)
     else:
-        logging.basicConfig(level = logging.ERROR)
+        logging.basicConfig(level=logging.ERROR)
     fs.init()
-    fs.main()  
+    fs.main()
